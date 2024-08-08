71c54a0f2040b9d9c377ba3050733126177681e2
Dušan Čolić <dcolic3121rn@raf.rs>
2024-08-08 14:13:27 +0200

    adds admin and user dtos

---
+++ UserService/src/main/java/com/flightmanager/UserService/dto/AdminDto.java
@@ -0,0 +1,17 @@
+package com.flightmanager.UserService.dto;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class AdminDto extends UserDto{
+
+}
--- UserService/src/main/java/com/flightmanager/UserService/dto/UserDto.java
+++ UserService/src/main/java/com/flightmanager/UserService/dto/UserDto.java
@@ -1,4 +1,19 @@
 package com.flightmanager.UserService.dto;
 
+import com.flightmanager.UserService.domain.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class UserDto {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Role role;
 }
