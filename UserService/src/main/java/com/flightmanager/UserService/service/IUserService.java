577a0282676d40f83028b6073e62a7e699fb35ae
Uroš Čolić <ucolic3021rn@raf.rs>
2024-08-08 14:51:29 +0200

    starts service layer

--- UserService/src/main/java/com/flightmanager/UserService/service/IUserService.java
+++ UserService/src/main/java/com/flightmanager/UserService/service/IUserService.java
@@ -1,4 +1,21 @@
 package com.flightmanager.UserService.service;
 
+import com.flightmanager.UserService.domain.RoleType;
+import com.flightmanager.UserService.domain.User;
+import com.flightmanager.UserService.dto.*;
+import org.springframework.data.domain.Page;
+
+import java.awt.print.Pageable;
+
 public interface IUserService {
+
+    Page<UserDto> findAll(Pageable pageable);
+    ClientDto findClientById(Long id);
+    ManagerDto findManagerById(Long id);
+    Page<UserDto> findAllOfRole(Pageable pageable, RoleType roleType);
+    ClientDto registerClient(ClientCreateDto clientDto);
+    ManagerDto registerManager(ManagerCreateDto managerDto);
+    ClientDto banClient(ClientBanDto clientBanDto);
+    ManagerDto banManager(ManagerBanDto managerBanDto);
+    TokenResponseDto login(TokenRequestDto tokenRequestDto);
 }
--- UserService/src/main/java/com/flightmanager/UserService/service/impl/UserServiceImpl.java
+++ UserService/src/main/java/com/flightmanager/UserService/service/impl/UserServiceImpl.java
@@ -1,14 +1,63 @@
 package com.flightmanager.UserService.service.impl;
 
+import com.flightmanager.UserService.domain.RoleType;
+import com.flightmanager.UserService.dto.*;
 import com.flightmanager.UserService.service.IUserService;
 import jakarta.transaction.Transactional;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+
+import java.awt.print.Pageable;
 
 @Getter
 @Setter
 @Service
 @Transactional
 public class UserServiceImpl implements IUserService {
+    @Override
+    public Page<UserDto> findAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public ClientDto findClientById(Long id) {
+        return null;
+    }
+
+    @Override
+    public ManagerDto findManagerById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Page<UserDto> findAllOfRole(Pageable pageable, RoleType roleType) {
+        return null;
+    }
+
+    @Override
+    public ClientDto registerClient(ClientCreateDto clientDto) {
+        return null;
+    }
+
+    @Override
+    public ManagerDto registerManager(ManagerCreateDto managerDto) {
+        return null;
+    }
+
+    @Override
+    public ClientDto banClient(ClientBanDto clientBanDto) {
+        return null;
+    }
+
+    @Override
+    public ManagerDto banManager(ManagerBanDto managerBanDto) {
+        return null;
+    }
+
+    @Override
+    public TokenResponseDto login(TokenRequestDto tokenRequestDto) {
+        return null;
+    }
 }
